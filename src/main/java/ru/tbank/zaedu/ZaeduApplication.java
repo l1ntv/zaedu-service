1d9311605418cf58ca861eb2c30046663cace03a
vlint <lintvadim@gmail.com>
2025-03-06 16:39:54 +0300

    Initial project

---
+++ src/main/java/ru/tbank/zaedu/ZaeduApplication.java
@@ -0,0 +1,13 @@
+package ru.tbank.zaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaeduApplication.class, args);
+	}
+
+}
