27baf88c110536cc672e3e4d0cbe1882f499d0e5
fedinairn08 <fedina_irn08@mail.ru>
2025-03-25 15:50:56 +0300

    Решены конфликты

--- src/main/java/ru/tbank/zaedu/service/OrderService.java
+++ src/main/java/ru/tbank/zaedu/service/OrderService.java
@@ -2,11 +2,15 @@ package ru.tbank.zaedu.service;
 
 
 import ru.tbank.zaedu.DTO.ClientsOrdersResponse;
-import java.security.Principal;
+import ru.tbank.zaedu.models.Order;
+
+import java.util.List;
 
 public interface OrderService {
 
     ClientsOrdersResponse findPlacedOrdersByClients(String masterLogin);
 
     void getPlacedOrder(Long id, String masterLogin);
+
+    List<Order> getClientOrders(String name);
 }
