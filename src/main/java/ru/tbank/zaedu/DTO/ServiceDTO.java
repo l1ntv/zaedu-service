0addd4cde21807c57621019502188e2d522d3cf8
vodoem <prosto_duz@mail.ru>
2025-03-26 21:54:32 +0300

    починил ручки masters и masters/{id}

--- src/main/java/ru/tbank/zaedu/DTO/ServiceDTO.java
+++ src/main/java/ru/tbank/zaedu/DTO/ServiceDTO.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class ServiceDTO {
     private Long id;
     private String serviceName;
